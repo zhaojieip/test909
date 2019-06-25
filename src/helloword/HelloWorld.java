ea967b0ff5d664ed2ed034cd10dc3e862546c9a4
zhaojieip <32030955+zhaojieip@users.noreply.github.com>
2019-06-25 16:53:58 +0800

    Update HelloWorld.java

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World  2019年6月25日16:46:58");
+		System.out.println("Hello World  2019年6月25日17:46:58");
 	}
 
 }
